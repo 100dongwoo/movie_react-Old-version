c07656007a9c82c919d08b722253e3ed11d8d774
dongwoo <qorehddn123@naver.com>
2020-07-18 02:24:55 +0900

    Add spa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Movie'
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const moives = [
+    {
+
+        title: "Matrix",
+        poster: "https://lh3.googleusercontent.com/proxy/dkz_Nc0PtpFAJNxctxBHQHAvthlizmHTALU_W7e-PmsrWR_If906BtFBBjyNZqFl7XgVzba6yoOtagJbjFhIg9bmW7GpZM9Vr8wr-1WrobKm_vUwwEEP0EFW" },
+    {
+        title: "Full metal Jack",
+        poster: "https://i.pinimg.com/originals/2f/69/68/2f696807a1f9f478fa82cc5faa69899e.jpg",
+    },
+    {
+        title: "Oldboy",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGh_DpO67G53MVL67jG7370u6P_X5AG5lhow&usqp=CAU"
+    },
+    {
+        title: "Star wars",
+        poster: "https://image.ytn.co.kr/general/jpg/2019/0705/201907051328313641_d.jpg"
+    }
+]
+
+class App extends Component {
+
+    state={
+        greeting:'hello'
+    }
+componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                greeting:'Hello again'
+            })
+        },5000)
+}
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.greeting}
+                {moives.map((movie, index) => {
+                    return <Movie title={movie.title} poster={movie.poster} key={index}/>
+                })}
+            </div>
+        )
+    }
+
 }
 
 export default App;
