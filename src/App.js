0d0975daf3a6a1dd727698e33b185aff792df3c7
dongwoo <qorehddn123@naver.com>
2020-07-19 16:08:55 +0900

    api-Checking_test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//15강 한번더봐보기
 import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
@@ -14,34 +15,13 @@ class App extends Component {
 // },1000 )
 // }
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                movies: [
-                    {
-                        title: "Matrix",
-                        poster: "https://lh3.googleusercontent.com/proxy/dkz_Nc0PtpFAJNxctxBHQHAvthlizmHTALU_W7e-PmsrWR_If906BtFBBjyNZqFl7XgVzba6yoOtagJbjFhIg9bmW7GpZM9Vr8wr-1WrobKm_vUwwEEP0EFW"
-                    },
-                    {
-                        title: "Full metal Jack",
-                        poster: "https://i.pinimg.com/originals/2f/69/68/2f696807a1f9f478fa82cc5faa69899e.jpg",
-                    },
-                    {
-                        title: "total_name",
-                        poster: "https://i.pinimg.com/originals/4a/d4/e6/4ad4e67b19d6e4c91877b317aed51f26.jpg",
-                    },
-                    {
-                        title: "Star wars",
-                        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSeXDkqMrFAg7I28ejQGw6dn3QwN8kTa1bEFw&usqp=CAU"
-                    },
-                    {
-                        title: "transingpoiong",
-                        poster: "https://image.ytn.co.kr/general/jpg/2019/0705/201907051328313641_d.jpg"
-
-                    }
-                ]
-
-            })
-        }, 5000)
+        fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+        //fetch url 에이젝스로 바꿀수있다 쉽게
+            .then(response=>response.json())
+            .then(json=>console.log(json))
+            .catch(err=>console.log(err))
+        // console.log(fetch('anime_api'))
+//respone으로체크로 하고 제이슨으로 변환하고 콘솔로 보는방식으로 한다 ,ㄴ
     }
 
     _renderMovies=()=>{
--- src/Movie.js
+++ src/Movie.js
@@ -1,30 +1,56 @@
-import React, {Component} from "react";
-
+import React from "react";
+import PropTypes from 'prop-types'
 import "./Movie.css"
 
-class Movie extends Component {
+// class Movie extends Component {
+//
 
+//
+//     render() {
+//
+//         return (
+//             // <div>
+//             //     <MoviePoster poster={this.props.poster}/>
+//             //     <h1>{this.props.title}</h1>
+//             // </div>
+//         )
+//     }
+// }
 
-    render() {
+//
+// class MoviePoster extends Component {
+//
+//     render() {
+//         console.log(this.props)
+//         return(
+//             <img src={this.props.poster} />)
+//
+//     }
+//
+// }
 
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="Movies Poster"/>
+    )
+}
 
-    render() {
-        console.log(this.props)
-        return(
-            <img src={this.props.poster} />)
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
 
-    }
-
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
